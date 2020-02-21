1f99196cc28875e6f32a4a4de2c7bfb31783fa0b
Warren Harper II <warrenharper2@att.net>
2020-02-21 21:36:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,16 +5,15 @@ class App < Sinatra::Base
     enable :sessions
     set :session_secret, "delta_zeta"
     end
-    
+
   get '/' do
     erb :index
   end
-  
+
   post '/checkout' do
   @sessions = session
   item = params["item"]
   @sessions[:item] = item
   end
-  
-end
 
+end
