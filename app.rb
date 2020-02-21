560b76d8affe2bf9322a33fe82bf5c85cb33e297
Warren Harper II <warrenharper2@att.net>
2020-02-21 21:54:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
-
 class App < Sinatra::Base
+
+    configure do
+    enable :sessions
+    set :session_secret, "delta_zeta"
+    end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+  @sessions = session
+  item = params["item"]
+  @sessions[:item] = item
+  end
+
 end
